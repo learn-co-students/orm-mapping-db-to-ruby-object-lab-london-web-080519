3ffe07db1843a37edf4dbe965c5da4300512c4ba
george-kirby <georgeskirby@btinternet.com>
2019-08-22 07:37:14 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,40 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
-
+  
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-  end
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+
+  end
+  
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -39,5 +61,26 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_9
+    self.all.select{|student| student.grade == "9"}
+  end
+
+  def self.students_below_12th_grade
+    self.all.select{|student| student.grade.to_i < 12}
+  end
+
+  def self.first_X_students_in_grade_10(number_of_students)
+    self.all.select{|student| student.grade == "10"}.slice(0, number_of_students)
+  end
+
+  def self.first_student_in_grade_10
+    self.all.select{|student| student.grade == "10"}.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    self.all.select{|student| student.grade == grade.to_s}
+    # binding.pry
   end
 end
